ac85c2e4c336ede89a7e6af576dae56d6ae0c58a
Hui Li <withlihui@gmail.com>
2017-01-29 21:49:25 +0800

    edit config info

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User, Role
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app(os.getenv('heroku'))
+app = create_app('heroku')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
